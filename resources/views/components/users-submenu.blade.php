99c1cb782a55c88b6d13df1347586639d78cdd5b
dimitrytar22 <dimitrytar22@gmail.com>
2024-11-05 00:42:41 +0200

    user submenu edited

--- resources/views/components/users-submenu.blade.php
+++ resources/views/components/users-submenu.blade.php
@@ -2,7 +2,12 @@
     <ul class="nav-links">
         <li><a href="{{route('admin.users.pets.index')}}">User's Pets</a></li>
         <li><a href="{{route('admin.users.inventories.index')}}">User's Inventories</a></li>
-        <li><a href="{{route('admin.users.registration_applications.index')}}">User's Registration Application <sup><span style="color: #ff0000">{{\App\Models\RegistrationApplication::all()->count()}}</span></sup></a></li>
+        <li><a href="{{route('admin.users.registration_applications.index')}}">User's Registration Application
+                @if(\App\Models\RegistrationApplication::all()->count() > 0)
+                    <sup><span
+                            style="color: #ff0000">{{\App\Models\RegistrationApplication::all()->count()}}</span></sup>
+                @endif</a>
+        </li>
 
     </ul>
 </nav>
